09deba1d1873b7d993e02ffad244c427c3335899
Mark Pekun <markpekun@Marks-MacBook-Pro.local>
2020-07-16 15:38:38 -0400

    Consuming data with ProductContext

--- src/App.js
+++ src/App.js
@@ -6,19 +6,19 @@ import data from "./data";
 import Navigation from "./components/Navigation";
 import Products from "./components/Products";
 import ShoppingCart from "./components/ShoppingCart";
-import productContext from "./contexts/productContext";
+import { ProductContext } from "./contexts/ProductContext";
 
 function App() {
   const [products] = useState(data);
   const [cart, setCart] = useState([]);
 
   const addItem = (item) => {
-    setCart(...cart, item);
+    setCart([...cart, item]);
   };
 
   return (
     <div className="App">
-      <productContext.Provider value={(products, addItem)}>
+      <ProductContext.Provider value={{ products, addItem }}>
         <Navigation cart={cart} />
 
         {/* Routes */}
@@ -29,7 +29,7 @@ function App() {
         <Route path="/cart">
           <ShoppingCart cart={cart} />
         </Route>
-      </productContext.Provider>
+      </ProductContext.Provider>
     </div>
   );
 }
